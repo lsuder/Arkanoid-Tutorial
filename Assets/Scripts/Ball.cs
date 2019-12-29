a270ea3d44bcc93fa83b60fd198baf967219c3dd
lsuder <lucas.suder@gmail.com>
2019-12-29 12:25:56 +0100

    Added a racket and a ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    // Movement Speed
+    public float speed = 100.0f;
+
+    // Use this for initialization
+    void Start() {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+}
